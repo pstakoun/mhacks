9311348e4f34d07e90fa113c3311cc43e57cc8b1
Peter Stakoun <peter@stakoun.com>
2017-09-22 21:38:15 -0400

    Oops

--- index.js
+++ index.js
@@ -8,22 +8,6 @@ app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 
-app.post("/gmail", (req, res) => {
-    // TODO
-});
-
-app.post("/calendar", (req, res) => {
-    // TODO
-});
-
-app.post("/trello", (req, res) => {
-    // TODO
-});
-
-app.post("/slack", (req, res) => {
-    // TODO
-});
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
