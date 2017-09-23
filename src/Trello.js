8d8b2c414ad1a5cab8a228d3d5cbe9d49a5c456c
yanlingycchen <yanlingycchen@gmail.com>
2017-09-22 21:56:05 -0400

    Initialized Trello Component

---
+++ src/Trello.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Trello extends Component {
+  //TODO Fetch data from Trello API
+  render() {
+    return (
+//TODO return Trello data to client
+    );
+  }
+}
+
+export default Trello;
