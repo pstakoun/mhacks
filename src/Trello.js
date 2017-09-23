d713d16271be7dbff1776d9d38f742ac296089d1
yanlingycchen <yanlingycchen@gmail.com>
2017-09-23 11:57:07 -0400

    Trello auth

--- src/Trello.js
+++ src/Trello.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 
 class Trello extends Component {
-  render() {
+  auth() {
     //TODO Authenticate via ajax()
     const opts = {
       name: "MHacks",
       success: function() {
-        // fetch board
+        // fetch board and get ID
+        var data = JSON.stringify(false);
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+        xhr.addEventListener("readystatechange", function () {
+          if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+          }
+        });
+        xhr.open("GET", "https://api.trello.com/1/members/id/boards");
+        xhr.send(data);
+        console.log(data);
+        var b = prompt("What board");
         //filter out completed tasks
-        window.Trello.get("boards/0OsfbJ6A/cards/open", function(cards) {
+        window.Trello.get("boards/"+b+"/cards/open", function(cards) {
           //sort by due date
           var due_cards = [];
           for (var i = 0; i < cards.length; i++) {
@@ -29,11 +41,13 @@ class Trello extends Component {
       }
     };
     window.Trello.authorize(opts);
-
+  }
+  render() {
     //TODO Fetch data from Trello API
     return (
       <div>
         <h2>Trello</h2>
+        <button onClick={this.auth.bind(this)}>Click me</button>
       </div>
     );
   }
