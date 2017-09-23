4ff82faaf9c8bf3d46825d2c0ca453c8d47c0b0c
yanlingycchen <yanlingycchen@gmail.com>
2017-09-23 15:03:22 -0400

    Actually fixed Trello

--- src/Trello.js
+++ src/Trello.js
@@ -9,26 +9,30 @@ class Trello extends Component {
         // Get member info
         window.Trello.get("members/me/boards", function(boards) {
           console.log(boards);
-          // Get boards 
+          // Get boards
           var due_cards = [];
+          var count = 0;
           for (var i = 0; i < boards.length; i++) {
             // Filter out completed tasks
-            window.Trello.get("boards/"+boards[i]+"/cards/open", function(cards) {
+            window.Trello.get("boards/"+boards[i].id+"/cards/open", function(cards) {
               // Sort by due date
               for (var i = 0; i < cards.length; i++) {
                 if (cards[i].due != null) {
                   due_cards.push(cards[i]);
                 }
               }
+              count++;
+              if (count == boards.length) {
+                due_cards.sort(function(a, b) {
+                  // Sort by date ascending
+                  var dateA = new Date(a.due), dateB = new Date(b.due);
+                  return dateA-dateB;
+                });
+                console.log(due_cards);
+                alert(due_cards[0].due);
+              }
             });
           }
-          due_cards.sort(function(a, b) {
-            // Sort by date ascending
-            var dateA = new Date(a.due), dateB = new Date(b.due);
-            return dateA-dateB;
-          });
-          console.log(due_cards);
-          alert(due_cards[0].due);
         });
       },
       error: function() {
