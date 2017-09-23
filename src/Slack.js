5f574deba82bb846a1b1a03e47e0aa8db38daf32
afahs <adam.fahs@gmail.com>
2017-09-23 19:28:19 -0400

    Revert "?"
    This reverts commit d0d40a0564051e04910ac8c3d0edf8e7c74a661b.

--- src/Slack.js
+++ src/Slack.js
@@ -20,6 +20,9 @@ class Slack extends Component {
         <h2>Slack</h2>
         <a href="https://slack.com/oauth/authorize?scope=identity.basic&client_id=68763704759.245695352610"><img alt="Sign in with Slack" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" /></a>
 <<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
         app.get("/auth/slack", (req, res) => {
         	var options = {
         			uri: 'https://slack.com/api/oauth.access?code='
@@ -42,6 +45,22 @@ class Slack extends Component {
         });}
 =======
 >>>>>>> parent of 562f347... Fix
+=======
+=======
+>>>>>>> parent of 1790c7f... ?
+        ?.get("https://slack.com/api/oauth.access", [uri: 'https://slack.com/api/oauth.access?code='
+  					+req.query.code+
+  					'&client_id=68763704759.245695352610'+
+  					'&client_secret=42202ad1b69296ae37695378e7d7993b'+
+  					'&redirect_uri=https://mhacksx.herokuapp.com',
+  			method: 'GET'] )
+<<<<<<< HEAD
+>>>>>>> parent of 1790c7f... ?
+=======
+>>>>>>> parent of 1790c7f... ?
+=======
+        ?.get("https://slack.com/api/oauth.access", [code, 68763704759.245695352610, 42202ad1b69296ae37695378e7d7993b] )
+>>>>>>> parent of 9223a70... ?
       </div>
     );
   }
