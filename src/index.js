0f66fd1a2b24910f0ab704d58c4458ff77334a1e
afahs <adam.fahs@gmail.com>
2017-09-23 18:59:15 -0400

    fiX?

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+const express = require("express");
+const app = express();
+const path = require("path");
+const request = require("request");
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+app.use(express.static(path.resolve(__dirname, "build")));
+
+app.get("/auth/slack", (req, res) => {
+	var options = {
+			uri: 'https://slack.com/api/oauth.access?code='
+					+req.query.code+
+					'&client_id=68763704759.245695352610'+
+					'&client_secret=42202ad1b69296ae37695378e7d7993b'+
+					'&redirect_uri=https://mhacksx.herokuapp.com',
+			method: 'GET'
+	};
+	request(options, (error, response, body) => {
+			var JSONresponse = JSON.parse(body)
+			if (!JSONresponse.ok){
+					console.log(JSONresponse)
+					res.send("Error encountered: \n"+JSON.stringify(JSONresponse)).status(200).end()
+			} else {
+					console.log(JSONresponse)
+					res.send("Success!")
+			}
+	});
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
