c799a2d9461659069b75efd8820f447bc01ca00f
Peter Stakoun <peter@stakoun.com>
2017-09-23 20:24:51 -0400

    Shit

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+  render() {
+    return (
+      <div className="Card">
+        <h2>{this.props.title}</h2>
+        <h3>{this.prop.subtitle}</h3>
+        <p>{this.props.desc}</p>
+      </div>
+    );
+  }
+}
+
+export default Card;
