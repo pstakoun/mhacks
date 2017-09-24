e339a569c3426029bb01dc1a938393ad5aa85e50
afahs <adam.fahs@gmail.com>
2017-09-23 21:35:37 -0400

    Test

--- src/Slack.js
+++ src/Slack.js
@@ -16,10 +16,30 @@ class Slack extends Component {
     //if it doesn't, return the message
     //otherwise, it's not an imperative and therefore not a command
     return (
-      <div>
+        <div>
         <h2>Slack</h2>
         <a href="https://slack.com/oauth/authorize?scope=identity.basic&client_id=68763704759.245695352610"><img alt="Sign in with Slack" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" /></a>
       </div>
+      app.get('/auth/redirect', (req, res) =>{
+    var options = {
+        uri: 'https://slack.com/api/oauth.access?code='
+            +req.query.code+
+            '&client_id='+process.env.CLIENT_ID+
+            '&client_secret='+process.env.CLIENT_SECRET+
+            '&redirect_uri='+process.env.REDIRECT_URI,
+        method: 'GET'
+    }
+    request(options, (error, response, body) => {
+        var JSONresponse = JSON.parse(body)
+        if (!JSONresponse.ok){
+            console.log(JSONresponse)
+            res.send("Error encountered: \n"+JSON.stringify(JSONresponse)).status(200).end()
+        }else{
+            console.log(JSONresponse)
+            res.send("Success!")
+        }
+    })
+})
     );
   }
 }
