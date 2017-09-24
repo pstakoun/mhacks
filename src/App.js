5a354fbcd8249296c68efc98edde0bad86776d72
afahs <adam.fahs@gmail.com>
2017-09-24 06:00:47 -0400

    Carousel
    No Content, Just copy paste

--- src/App.js
+++ src/App.js
@@ -67,26 +67,63 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h1>MHacks</h1>
+<div id="text-carousel" class="carousel slide" data-ride="carousel">
+    <div class="row">
+        <div class="col-xs-offset-3 col-xs-6">
+            <div class="carousel-inner">
+                <div class="item active">
+                    <div class="carousel-content">
+                        <div>
+                            <p>Sapiente, ducimus, voluptas, mollitia voluptatibus nemo explicabo sit blanditiis laborum dolore illum fuga veniam quae expedita libero accusamus quas harum ex numquam necessitatibus provident deleniti tenetur iusto officiis recusandae corporis culpa quaerat?</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="carousel-content">
+                        <div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, sint fuga temporibus nam saepe delectus expedita vitae magnam necessitatibus dolores tempore consequatur dicta cumque repellendus eligendi ducimus placeat! </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="carousel-content">
+                        <div>
+                            <p>Sapiente, ducimus, voluptas, mollitia voluptatibus nemo explicabo sit blanditiis laborum dolore illum fuga veniam quae expedita libero accusamus quas harum ex numquam necessitatibus provident deleniti tenetur iusto officiis recusandae corporis culpa quaerat?</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
-        <ul className="cards">
-          {this.renderCards()}
-        </ul>
-        <br></br>
-        <ul className="integrations">
-          <li>
-            <Google setCalendarCards={this.setCalendarCards.bind(this)} setGmailCards={this.setGmailCards.bind(this)} addGmailCard={this.addGmailCard.bind(this)} />
-          </li>
-          <li>
-            <Trello setTrelloCards={this.setTrelloCards.bind(this)} />
-          </li>
-          <li>
-            <Slack setSlackCards={this.setSlackCards.bind(this)} />
-          </li>
-        </ul>
-      </div>
+    </div>
+    <a class="left carousel-control" href="#text-carousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+ <a class="right carousel-control" href="#text-carousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+  <div className="App">
+    <div className="App-header">
+      <h1>MHacks</h1>
+    </div>
+    <ul className="cards">
+      {this.renderCards()}
+    </ul>
+    <br></br>
+    <ul className="integrations">
+      <li>
+        <Google setCalendarCards={this.setCalendarCards.bind(this)} setGmailCards={this.setGmailCards.bind(this)} addGmailCard={this.addGmailCard.bind(this)} />
+      </li>
+      <li>
+        <Trello setTrelloCards={this.setTrelloCards.bind(this)} />
+      </li>
+      <li>
+        <Slack setSlackCards={this.setSlackCards.bind(this)} />
+      </li>
+    </ul>
+  </div>
+</div>
+
     );
   }
 }
